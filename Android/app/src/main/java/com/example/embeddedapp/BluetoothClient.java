a1577babaed61b2f29cb978fb4e2bd2e91ba163b
YoungHyeon1 <73692488+YoungHyeon1@users.noreply.github.com>
2023-06-13 02:18:43 +0900

    bluetooth connect add & mcu view add

---
+++ Android/app/src/main/java/com/example/embeddedapp/BluetoothClient.java
@@ -0,0 +1,72 @@
+package com.example.embeddedapp;
+
+
+import static androidx.constraintlayout.widget.ConstraintLayoutStates.TAG;
+
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.os.Bundle;
+import android.os.Message;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
+public class BluetoothClient extends Thread {
+    private final BluetoothSocket mmSocket;
+    private final BluetoothDevice mmDevice;
+
+    private byte[] mmBuffer; // mmBuffer store for the stream
+
+    public BluetoothClient(BluetoothDevice device) {
+        // Use a temporary object that is later assigned to mmSocket
+        // because mmSocket is final.
+        BluetoothSocket tmp = null;
+        mmDevice = device;
+
+        try {
+            // Get a BluetoothSocket to connect with the given BluetoothDevice.
+            // MY_UUID is the app's UUID string, also used in the server code.
+            tmp = device.createRfcommSocketToServiceRecord(UUID.fromString(""));
+        } catch (IOException e) {
+            Log.e(TAG, "Socket's create() method failed", e);
+        } catch (SecurityException e) {
+        }
+        mmSocket = tmp;
+    }
+
+    public void run() {
+        // Cancel discovery because it otherwise slows down the connection.
+//        bluetoothAdapter.cancelDiscovery();
+
+        try {
+            // Connect to the remote device through the socket. This call blocks
+            // until it succeeds or throws an exception.
+            mmSocket.connect();
+        } catch (IOException connectException) {
+            // Unable to connect; close the socket and return.
+            try {
+                mmSocket.close();
+            } catch (IOException closeException) {
+                Log.e(TAG, "Could not close the client socket", closeException);
+            }
+            return;
+        } catch (SecurityException e) {
+        }
+
+        // The connection attempt succeeded. Perform work associated with
+        // the connection in a separate thread.
+//        manageMyConnectedSocket(mmSocket);
+    }
+
+    // Closes the client socket and causes the thread to finish.
+    public void cancel() {
+        try {
+            mmSocket.close();
+        } catch (IOException e) {
+            Log.e(TAG, "Could not close the client socket", e);
+        }
+    }
+}
